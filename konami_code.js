44037cb8b8e0942cb077c5a2f85fd83ec55acf67
Michael Iasiello <maiasiel@ncsu.edu>
2018-06-10 23:23:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,30 @@ const codes = [
   "a"
 ];
 
+index = 0;
+
 function init() {
-  // your code here
+  keysDown = [];
+  document.addEventListener('keydown', function (e) {
+    var key = e.key;
+    keysDown[index] = key
+    var requiredKey = codes[index];
+    
+    if (key === requiredKey) {
+      index++
+      if (index === codes.length){
+        alert("Cheat codes activated")
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  });
+ 
 }
+
+function returnKeysDown() {
+  console.log(keysDown)
+}
+
